e78f82f1276aba86c15834dbcd66f1309bb179ce
JahnaviKrishnan <jahnavi.krishnan@s.amity.edu>
2025-05-03 19:44:40 +0530

    merge req

---
+++ merge.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void add(int,int);
+int main{
+	        int a,b;
+		        int a=15;
+			        int b=10;
+				        void add(a,b);
+}
+void add(int a,int b)
+{
+	        int c;
+		        c=a+b;
+			        return c;
+}
+
