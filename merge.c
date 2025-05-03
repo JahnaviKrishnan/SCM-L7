778e19170606f89901f76c17f58d6ad2650c97e8
JahnaviKrishnan <jahnavi.krishnan@s.amity.edu>
2025-05-03 19:39:02 +0530

    merge req

---
+++ merge.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void add(int,int);
+int main{
+	int a,b;
+	int a=5;
+	int b=10;
+	void add(a,b);
+}
+void add(int a,int b)
+{ 
+	int c;
+	c=a+b;
+	return c;
+}
---
+++ scm
@@ -0,0 +1,2 @@
+hello sir, my laptop is under repair so i am not able to work on my project
+
